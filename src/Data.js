f5fa041a093b943fbe44c6b9c43956e446e0afd1
Nicholas Finch <nicfinch223@gmail.com>
2020-05-20 11:17:55 -0400

    most is done, have to add projects

---
+++ src/Data.js
@@ -0,0 +1,58 @@
+export default
+    {
+        
+        name :'Nicholas Finch',
+        headerTagline: [
+            'Full-Stack Web Developer from',
+            'Greensboro, North Carolina'
+          ],
+        contactEmail:'nicfinch223@gmail.com',
+        abouttext: "I had originally gotten my Associate's degree in IT from a local Community College. After little success in the job hunt, I decided to further my education with the UNC coding bootcamp. Currently, I am a server at Red Robin and have been for 2 years. Although I think writing code is a good challenge my dream job would be in music production. I love playing the piano and I am very interested in learning more instruments. I am a deeply motivated programmer, I love learning new things and continuing to challenge myself.",
+        aboutImage: 'https://avatars2.githubusercontent.com/u/57764795?s=400&u=4be80bacf42ba1b0d47a387ebdf1df1081456dcd&v=4',
+
+       ShowAboutImage:true,
+
+       projects:[
+           {
+            id: 1,
+            title:'Project One', 
+             service:'', 
+             imageSrc:'',
+             url:''
+            },
+            {
+                id: 2,
+                title: 'Project Two',
+                service: '',
+                imageSrc: '',
+                url: ''
+            },
+            { 
+                id: 3,
+                title: 'Project Three',
+                service: '',
+                imageSrc: "",
+                url: ''
+            },
+            {
+                id: 4,
+                title: 'Project Four',
+                service: '',
+                imageSrc: "",
+                url: ''
+           }
+        ],
+        social: [
+            {   name: 'Github',
+                url:'https://github.com/Finch23'
+            },
+            {
+                name: 'LinkedIn',
+                url: 'https://www.linkedin.com/in/nicholas-finch-506447185/'
+            },
+            {
+                name: 'Resume',
+                url: 'https://drive.google.com/open?id=1Je12-CQLFTwxwsYBAq6eqsIxqwPmt2rg'
+            }
+        ]
+    }
