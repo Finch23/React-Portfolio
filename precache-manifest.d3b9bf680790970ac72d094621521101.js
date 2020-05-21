a361d273b6daf9b8b308d641f6c10caf3750e07e
Nicholas Finch <nicfinch223@gmail.com>
2020-05-21 15:26:22 -0400

    Updates

---
+++ precache-manifest.d3b9bf680790970ac72d094621521101.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80035557ea1c1083d85eec9772604555",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "1c448540f03a24f07fe9",
+    "url": "/React-Portfolio/static/css/main.0d3a6199.chunk.css"
+  },
+  {
+    "revision": "dc6e909e1ffcb46cfede",
+    "url": "/React-Portfolio/static/js/2.2c7ae1b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.2c7ae1b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c448540f03a24f07fe9",
+    "url": "/React-Portfolio/static/js/main.c514e561.chunk.js"
+  },
+  {
+    "revision": "c8315fd2c0dd648bcebb",
+    "url": "/React-Portfolio/static/js/runtime-main.f1ce8f8d.js"
+  }
+]);
