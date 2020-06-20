ea46461abdf0babf110823e85d993c575ab96154
Nicholas Finch <nicfinch223@gmail.com>
2020-06-20 15:18:06 -0400

    Updates

--- precache-manifest.a59b8991868ab8026c3ecb4a8759b6f9.js
+++ precache-manifest.a59b8991868ab8026c3ecb4a8759b6f9.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2faa0777cac42394c1e090fe75ec4f2c",
+    "revision": "0ac78c50e1990e847098029fd2e9e1f2",
     "url": "/React-Portfolio/index.html"
   },
   {
-    "revision": "d15cbda149bc23b69299",
-    "url": "/React-Portfolio/static/css/main.ef507384.chunk.css"
+    "revision": "46ce384f5547ffc8fe08",
+    "url": "/React-Portfolio/static/css/main.1423379c.chunk.css"
   },
   {
     "revision": "dc6e909e1ffcb46cfede",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/React-Portfolio/static/js/2.2c7ae1b3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d15cbda149bc23b69299",
-    "url": "/React-Portfolio/static/js/main.4094c087.chunk.js"
+    "revision": "46ce384f5547ffc8fe08",
+    "url": "/React-Portfolio/static/js/main.6163499f.chunk.js"
   },
   {
     "revision": "c8315fd2c0dd648bcebb",
